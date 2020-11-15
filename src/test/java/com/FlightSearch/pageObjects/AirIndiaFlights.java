be50a444b2aade541f4f5bde5ca86c0f64f728a7
athanishreedhar <shridhara1213@gmail.com>
2020-11-15 23:33:40 +0530

    Branch created and pushinng first code

---
+++ src/test/java/com/FlightSearch/pageObjects/AirIndiaFlights.java
@@ -0,0 +1,7 @@
+package com.FlightSearch.pageObjects;
+
+public class AirIndiaFlights {
+	public void name() {
+		System.out.println("hi");
+	}
+}
---
+++ src/test/java/com/FlightSearch/testCases/ServiceBase.java
@@ -0,0 +1,7 @@
+package com.FlightSearch.testCases;
+
+public class ServiceBase {
+public void name() {
+	System.out.println("hi");
+}
+}
