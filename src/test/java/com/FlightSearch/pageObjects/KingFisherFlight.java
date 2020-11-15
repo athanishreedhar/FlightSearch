3dfe6d6bb35baa1212e5de8d99f26b38e3da4af7
athanishreedhar <shridhara1213@gmail.com>
2020-11-15 23:41:27 +0530

    B guy adding code to FW

---
+++ src/test/java/com/FlightSearch/pageObjects/KingFisherFlight.java
@@ -0,0 +1,10 @@
+package com.FlightSearch.pageObjects;
+
+public class KingFisherFlight {
+
+	public static void main(String[] args) {
+		System.out.println("Mallya");
+
+	}
+
+}
---
+++ src/test/java/com/FlightSearch/testCases/BaseHooks.java
@@ -0,0 +1,10 @@
+package com.FlightSearch.testCases;
+
+public class BaseHooks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
