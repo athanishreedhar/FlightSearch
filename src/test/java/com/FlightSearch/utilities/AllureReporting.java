e75d14ca6a5bbf7dfd9ead2925d07ddf7b9df5fd
athanishreedhar <shridhara1213@gmail.com>
2020-11-16 12:54:37 +0530

    deleted utility files by B guy

--- src/test/java/com/FlightSearch/utilities/AllureReporting.java
+++
@@ -1,7 +0,0 @@
-package com.FlightSearch.utilities;
-
-public class AllureReporting {
-public void name() {
-	System.out.println("allu");
-}
-}
--- src/test/java/com/FlightSearch/utilities/ExtentReporting.java
+++
@@ -1,9 +0,0 @@
-package com.FlightSearch.utilities;
-
-public class ExtentReporting {
-
-	private void pub() {
-		System.out.println("ext");
-
-	}
-}
