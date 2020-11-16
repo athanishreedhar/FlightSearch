7f1b29c1127d349705522583c9b68fdd9c5e4c36
athanishreedhar <shridhara1213@gmail.com>
2020-11-16 12:08:23 +0530

    B guy enhanced test cases on day 2

---
+++ src/test/java/com/FlightSearch/pageObjects/PrivateJets.java
@@ -0,0 +1,8 @@
+package com.FlightSearch.pageObjects;
+
+public class PrivateJets {
+
+	public void name() {
+		System.out.println("Jet");
+	}
+}
---
+++ src/test/java/com/FlightSearch/testCases/TC_SearchprivateJets.java
@@ -0,0 +1,10 @@
+package com.FlightSearch.testCases;
+
+public class TC_SearchprivateJets {
+
+	public static void main(String[] args) {
+		System.out.println("private jets");
+
+	}
+
+}
