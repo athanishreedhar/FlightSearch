1febb1125ccdb77fc3f8dbd6da05d3d0fd9c9d49
athanishreedhar <shridhara1213@gmail.com>
2020-11-16 13:47:00 +0530

    adding new test cases and methods

---
+++ src/test/java/com/FlightSearch/testCases/TC_SearchForAirIndia.java
@@ -0,0 +1,11 @@
+package com.FlightSearch.testCases;
+
+public class TC_SearchForAirIndia {
+
+	public void A1() {
+		System.out.println("m1");
+	}
+	public void A2() {
+		System.out.println("m1");
+	}
+}
--- src/test/java/com/FlightSearch/testCases/TC_SearchForCommercialFlights.java
+++ src/test/java/com/FlightSearch/testCases/TC_SearchForCommercialFlights.java
@@ -6,5 +6,16 @@ public class TC_SearchForCommercialFlights {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public void m1() {
+		System.out.println("m1");
+	} 
+	
+	public void m2() {
+		System.out.println("m2");
+	}
+	
+	public void m3() {
+		System.out.println("m3");
+	}
 }
