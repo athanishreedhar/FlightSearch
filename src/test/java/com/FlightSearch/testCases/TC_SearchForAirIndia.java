4ef697d813fa2ab0c862e3b0ba6241bbde7395e7
athanishreedhar <shridhara1213@gmail.com>
2020-11-16 13:57:18 +0530

    A guy added methos to test case

--- src/test/java/com/FlightSearch/testCases/TC_SearchForAirIndia.java
+++ src/test/java/com/FlightSearch/testCases/TC_SearchForAirIndia.java
@@ -8,4 +8,14 @@ public class TC_SearchForAirIndia {
 	public void A2() {
 		System.out.println("m1");
 	}
+	
+	public void A3() {
+		System.out.println("m1");
+	}
+	public void A4() {
+		System.out.println("m1");
+	}
+	public void A5() {
+		System.out.println("m1");
+	}
 }
