14d87c0252dd21eb47ff2f41ef3bc0310d833567
athanishreedhar <shridhara1213@gmail.com>
2020-11-16 13:53:25 +0530

    Helpr methods are added by B guy

--- src/test/java/com/FlightSearch/utilities/Helper.java
+++ src/test/java/com/FlightSearch/utilities/Helper.java
@@ -9,4 +9,16 @@ public class Helper {
 	public void H2() {
 		System.out.println("m1");
 	}
+	
+	public void H3() {
+		System.out.println("m1");
+	}
+	
+	public void H4() {
+		System.out.println("m1");
+	}
+	
+	public void H5() {
+		System.out.println("m1");
+	}
 }
